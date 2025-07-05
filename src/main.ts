aa1a7b5fcf5142813e84cf5010e782ac583b7401
lordydill <ahamdfadil846@gmail.com>
2025-07-05 21:29:46 +0700

    crm: PT bayer munich

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('PT Bayer Munich') // Judul API
-    .setDescription('API untuk Praktek Pelanggan - Ahmad Fadil & Ade Budi Setiawan (233657201002)')
-    .setVersion('1.0') // Versi aplikasi
+    .setTitle('PT bayer munich')
+    .setDescription(
+      'API untuk Praktek Pelanggan - Ahmad Fadil & Ade Budi Setiawan (233657201002)',
+    )
+    .setVersion('1.0')
     .addBearerAuth(
       {
         type: 'http',
